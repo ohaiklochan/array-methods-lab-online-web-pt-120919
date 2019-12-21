7931249c52943ab9c45baf936646b5a89d662b3f
Chloe Foronda <caforonda103@yahoo.com>
2019-12-21 15:07:59 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,30 +1,37 @@
 def using_include
   the_office = ["Michael","Jim", "Dwight", "Pam"]
   the_office.include?("Ryan")
+  => false
   the_office.include?("Dwight")
+  => true
 end
 
 def using_sort
   the_office = ["Michael","Jim", "Dwight", "Pam"]
   the_office.sort
+  => "Dwight", "Jim", "Michael", "Pam"
 end
 
 def using_reverse
   the_office = ["Michael","Jim", "Dwight", "Pam"]
   the office.reverse
+  => "Pam", "Dwight", "Jim", "Michael"
 end
 
 def using_first
   the_office = ["Michael","Jim", "Dwight", "Pam"]
   the_office.first 
+  => "Michael"
 end
 
 def using_last
   the_office = ["Michael","Jim", "Dwight", "Pam"]
   the_office.last
+  => "Pam"
 end
 
 def using_size
   the_office = ["Michael","Jim", "Dwight", "Pam"]
   the_office.size
+  => 4
 end
