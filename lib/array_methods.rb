2b8ba53e507a1460d6f1a5f5d7c85d1507ff2f90
Chloe Foronda <caforonda103@yahoo.com>
2019-12-21 14:46:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,15 +4,17 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-
+  the_office = ["Michael","Jim", "Dwight", "Pam"]
+  the_office.sort
 end
 
 def using_reverse(array)
-
+  the_office = ["Michael","Jim", "Dwight", "Pam"]
+  the office.reverse
 end
 
 def using_first(array)
-
+  
 end
 
 def using_last(array)
